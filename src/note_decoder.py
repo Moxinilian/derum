fd7d11ea60e6a11bb794088940d13e7916225545
Théo Degioanni <theo.degioanni@ens-rennes.fr>
2021-09-22 23:55:35 +0200

    added release filter generator

--- src/note_decoder.py
+++ src/note_decoder.py
@@ -4,10 +4,12 @@ import ddsp
 
 from utils import right_handed_conv
 
-# ADSR controls: [a, d, s, r, aslope, rslope]
+# ADSR controls: [a (secs), d (secs), s, r (secs), aslope, rslope]
 N_ADSR_CONTROL = 6
 
 DECAY_SLOPE = 8
+
+# TODO: Support batches larger than 1
 
 def build_attack_decay_filter(adsr_control, attack_decay_filter_len, automation_rate):
     a = adsr_control[:,0]
@@ -23,6 +25,14 @@ def build_attack_decay_filter(adsr_control, attack_decay_filter_len, automation_
         ad_filter_concat.append(tf.reshape(v, (v.shape[0], 1)))
     return tf.concat(ad_filter_concat, axis=1)
 
+def build_release_filter(adsr_control, release_filter_len, automation_rate):
+    s = adsr_control[:,2]
+    r = adsr_control[:,3]
+    rslope = adsr_control[:,5]
+    t = tf.repeat(tf.range(release_filter_len, dtype=tf.float32) / automation_rate, adsr_control.shape[0])
+    t = tf.reshape(t, (adsr_control.shape[0], release_filter_len))
+    print(t)
+    return tf.nn.relu(s * (1 - (tf.exp(rslope/r * t) - 1) / (tf.exp(rslope) - 1)))
 
 def global_envelope_v2(notes, adsr_control, automation_len, automation_rate):
     """
